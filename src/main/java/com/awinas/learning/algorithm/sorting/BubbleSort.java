4407a1f1c540f7108e49cb8f09242e93e872c16b
Awinas Kannan M R <awinas270597@gmail.com>
2025-02-14 13:58:01 +0530

    Bubble Sort

--- src/main/java/com/awinas/learning/algorithm/sorting/BubbleSort.java
+++ src/main/java/com/awinas/learning/algorithm/sorting/BubbleSort.java
@@ -2,32 +2,51 @@ package com.awinas.learning.algorithm.sorting;
 
 import java.util.Arrays;
 
-//isSwapped -> if there were no changes in iteration
+/*
+ * Move the Largest element to the last
+ *  
+ * Repeatedly swaps adjacent elements if they are in the wrong order.
+ * 		Time Complexity: O(n²)	
+ * 		Best for: Small datasets, easy to implement.
+ * 
+ * Optimized Version
+ * 		The swapped flag is used to stop the algorithm if no swaps occur in a full pass (indicating the array is already sorted).
+ * 		Reduces unnecessary iterations in best-case scenarios.
+ * 
+ * 	
+ */
 
 public class BubbleSort {
 
 	public static void main(String[] args) {
 		int arr[] = { 64, 34, 25, 12, 22, 11, 90 };
-		// int arr[] = { 11, 12, 22, 25, 34, 64, 90 };
 
-		for (int i = 0; i < arr.length - 1; i++) {
+		System.out.println(Arrays.toString(bubbleSort(arr)));
+		System.out.println(Arrays.toString(bubbleSort(new int[] { 11, 12, 22, 25, 34, 64, 90, 100 })));
+	}
+
+	private static int[] bubbleSort(int[] array) {
+
+		for (int i = 0; i < array.length - 1; i++) {
 			boolean isSwapped = false;
-			System.out.println("Here");
-			for (int j = 0; j < arr.length - 1 - i; j++) {
-				if (arr[j] > arr[j + 1]) {
+			System.out.println("Loop " + (i + 1));
+			for (int j = 0; j < array.length - 1 - i; j++) {
+				if (array[j] > array[j + 1]) {
 					isSwapped = true;
-					arr[j] = arr[j] + arr[j + 1];
-					arr[j + 1] = arr[j] - arr[j + 1];
-					arr[j] = arr[j] - arr[j + 1];
+					array[j] = array[j] + array[j + 1];
+					array[j + 1] = array[j] - array[j + 1];
+					array[j] = array[j] - array[j + 1];
 				}
 			}
+
+			// If no swaps happened, the array is already sorted
 			if (!isSwapped) {
+				System.out.println("Fully Sorted");
 				break;
 			}
 
 		}
-
-		System.out.println(Arrays.toString(arr));
+		return array;
 	}
 
 }
