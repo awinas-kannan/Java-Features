48db006621bc99bbbe3c415e4fa8e79f6c720126
Awinas Kannan M R <awinas270597@gmail.com>
2025-02-14 16:46:03 +0530

    insertion sort
    insertion sort

--- src/main/java/com/awinas/learning/algorithm/sorting/InsertionSort.java
+++ src/main/java/com/awinas/learning/algorithm/sorting/InsertionSort.java
@@ -7,37 +7,45 @@ public class InsertionSort {
 	public static void main(String[] args) {
 
 		int arr[] = { 64, 34, 25, 12, 22, 11, 90, 0 };
-		// int arr[] = { 12, 11, 13, 5, 6 };
-		int n = arr.length;
 
+		System.out.println(Arrays.toString(insertionSort(arr)));
+		System.out.println(Arrays.toString(insertionSort(new int[] { 12, 11, 13, 5, 6 })));
+
+	}
+
+	/*
+	 * Start from second number
+	 * 
+	 * Check from 0 index till Current number index
+	 * 
+	 * If number at i (fixed ) is greater the number at j, then move the number at i
+	 * to j'th position shift all the numbers from j'th position till i
+	 * 
+	 */
+	
+	private static int[] insertionSort(int[] array) {
 		// Start from second number
-		for (int i = 1; i < n; i++) {
+		for (int i = 1; i < array.length; i++) {
 			// Check from 0 index till Current number index
 			for (int j = 0; j < i; j++) {
-				// If Our Current number is less than any of prev number
-				// then move out current number to that position
-				// then shift all the number from that pos to below
-				if (arr[i] < arr[j]) {
-					shift(i, j, arr);
+				if (array[i] < array[j]) {
+					shift(i, j, array);
 					break;
 				}
 			}
+
 		}
-
-		System.out.println(Arrays.toString(arr));
+		return array;
 	}
-
+	
+	
 	private static void shift(int i, int j, int[] arr) {
-		System.out.println("Current number " + arr[i]);
-		System.out.println("Lesser than number " + arr[j]);
-		System.out.println(" Start " + j + " End " + i);
 		int temp = arr[i];
 		for (int x = i; x > j; x--) {
 			arr[x] = arr[x - 1];
 		}
 		arr[j] = temp;
 
-		System.out.println(Arrays.toString(arr));
 	}
 
 }
