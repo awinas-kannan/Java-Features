1ea659a9f71edf5ccc43cfa1127738d299f86e7b
Awinas Kannan M R <awinas270597@gmail.com>
2025-02-14 14:26:43 +0530

    Selection Sort
    Selection Sort

--- src/main/java/com/awinas/learning/algorithm/sorting/SelectionSort.java
+++ src/main/java/com/awinas/learning/algorithm/sorting/SelectionSort.java
@@ -2,30 +2,35 @@ package com.awinas.learning.algorithm.sorting;
 
 import java.util.Arrays;
 
+/*
+ * 	Finds the minimum element index and places it in the correct position iteratively.
+ * 
+ * 	Time Complexity: O(n²)
+ * 	Best for: Small datasets.
+ 
+ */
 public class SelectionSort {
 
 	public static void main(String[] args) {
-		int arr[] = { 64, 34, 25, 12, 22, 11, 90, 0 };
-		int n = arr.length;
-		for (int i = 0; i < n - 1; i++) {
-			int min_index = i;
-			for (int j = i + 1; j < n; j++) {
-				if (arr[min_index] > arr[j]) {
-					min_index = j;
+		int arr[] = { 64, 34, 25, 12, 22, 11, 90, 0, -1, -2 };
+		System.out.println(Arrays.toString(selectionSort(arr)));
+	}
+
+	private static int[] selectionSort(int[] array) {
+
+		for (int i = 0; i < array.length - 1; i++) {
+			int minIndex = i; // Assume the first element is the smallest
+
+			// Find the index of the minimum element
+			for (int j = i + 1; j < array.length; j++) {
+				if (array[j] < array[minIndex]) {
+					minIndex = j;
 				}
 			}
 
-//			Works
-//			for (int j = i ; j < n-1; j++) {
-//				if (arr[min_index] > arr[j+1]) {
-//					min_index = j+1;
-//				}
-//			}
-
-			swap(i, min_index, arr);
+			swap(i, minIndex, array);
 		}
-
-		System.out.println(Arrays.toString(arr));
+		return array;
 	}
 
 	private static void swap(int i, int min_index, int[] arr) {
